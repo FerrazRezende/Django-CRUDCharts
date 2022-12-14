9e4d8e5f7a45f0a34f044ea6f8eb05d5c76910b1
Matheus Ferraz <107080615+FerrazRezende@users.noreply.github.com>
2022-12-14 18:36:52 -0300

    DB Postgres
    Change DB to PostgreSQL

--- Django_CRUDCharts/Django_CRUDCharts/settings.py
+++ Django_CRUDCharts/Django_CRUDCharts/settings.py
@@ -63,9 +63,13 @@ WSGI_APPLICATION = 'Django_CRUDCharts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crud',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5433'
+        }
 }
 
 
@@ -100,12 +104,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
